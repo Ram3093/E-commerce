0efd9224102a11fe9b50b64ffe5d4269dea4bf2f
Ram3093 <65605721+Ram3093@users.noreply.github.com>
2020-07-18 22:37:15 +0530

    create cart.js

--- src/components/Navigation/Links/cart.js
+++ src/components/Navigation/Links/cart.js
@@ -3,38 +3,51 @@ import React, { Component } from 'react'
 
 
 
-const arrayImage=[];
-console.log(localStorage.getItem('url'))
+//const arrayImage=[];
+//console.log(localStorage.getItem('url'))
 
 
 
 class Cart extends Component {
-    render() {
+  
+    render()
+    {
+        const sessionItem=JSON.parse(sessionStorage.getItem('products'))
+        console.log(sessionItem)   
+        console.log(typeof(sessionItem))  
+        
+        const sessionProducts=sessionItem ? sessionItem.map((el)=>{
+            return (
+               
+            <div className='row card' style={{marginTop: "30px" }}>
+              <div className='col l6  m12 s12' style={{padding:"30px"}}>
+                               <img src={el.Url}  width="300" height="300" alt='random' />
+                          </div>
+
+                    <div className="col l3  m12 s12" >
+                            <div style={{paddingTop:" 80px", paddingLeft:"10px" }}>
+                                   <p style={{ boxSizing:"border-box",fontSize:"1.5rem",fontWeight:"bolder",cursor:"pointer"  }}>{el.productName}</p>
+                                   <p style={{ display:"inlineBlock", marginTop:"15px", fontSize:"25px",fontWeight:"bolder",cursor:"pointer"}}><i className="fas fa-rupee-sign"> &nbsp;{el.productPrice}</i></p>
+                            </div>
+                    </div>
+
+
+                    <div className='col l3  m12 s12'>
+                      <p style={{paddingTop: "115px" ,paddingLeft:"75px", fontWeight:"bolder" }} ><i onClick={()=>{}} className="material-icons" style={{fontSize:"2.5rem",cursor:"pointer"}}>close</i> </p> 
+                    </div>
+             </div>          
+            
+        )
+            })
+      :<h4 className='center red-text'>No item added to card</h4>
         
         return (
+            <div className='container' style={{padding: "0 200px"}}>
+                     { sessionProducts }
+            </div>
             // <AuthUserContext.Consumer>
             // {authUser => (
-                <div>
-                   <form style={{width: '50%',margin: '30px auto', border: '1px solid #CCC' , borderRadius: '0.5em'}}>
-	                     <div className="cart" style={{margin:'60px 10px'}}>
-		                       <div className="row" style={{marginTop:'3rem' }}>
-			                        <div className="col-1" style={{width:'50%', float: 'left'}}>
-				                        <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTGOXKAgST8gfuou8g9rtGpWSrwEMS_D_kGKrsuj3IRpHy7h5jzV_UJp2OczmbRV3D7bjszgw-_dxg&usqp=CAc" width="200px" style={{paddingBottom: '20px'}} alt=""/>
-			                            <span style={{float: 'right', paddingRight: '70px'}}>Product name</span>
-                                    </div>
-			                        <div className=" col-2" style={{ width:'50%', float: 'left', paddingTop: '50px' }}>
-	                                   
-		                                 <span><button className='btn grey lighten-4' style={{float:'right'}}><img src="https://img.icons8.com/material-two-tone/2x/delete-sign.png" width="10px" alt="" /></button></span>
-		                            </div>
-	                            </div>
-                               <hr style={{width:'90%',  border: '1px solid #CCC', borderBottom: '0px' }} />
-	                           
-                            </div>
-                            
- 
-                    </form>
-        
-                </div>
+               
             // )}
             // </AuthUserContext.Consumer>
         )
